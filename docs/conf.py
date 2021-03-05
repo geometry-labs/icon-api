db50647cf60c9f0db2fd249d79f3ac94e1977f4e
Richard Mah <dick.mah@gmail.com>
2021-03-05 11:45:36 -0500

    Update docs conf.py to properly render markdown tables

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,8 @@ author = 'Geometry Labs, Inc.'
 # ones.
 extensions = [
     'sphinxcontrib.openapi',
-    'recommonmark'
+    'recommonmark',
+    'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
